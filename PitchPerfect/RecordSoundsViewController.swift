81523150805c267aa7d9760c7b271408e685b004
Dustin M <logins.dmahone@gmail.com>
2019-08-26 20:47:49 -0400

    first commit

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Dustin on 8/19/19.
+//  Copyright © 2019 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+    }
+
+}
+
